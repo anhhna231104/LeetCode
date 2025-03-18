f93fc4b3d3e3c1c9f1a7391cf825d7b1c79b2249
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-03-18 17:50:45 +0700

    Time: 0 ms (100%), Space: 12.4 MB (80.44%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        k = 0
-        for num in nums:
-            if num!=val:
-                nums[k] = num
-                k+=1
+        k = len(nums)
+        i = 0
+        while i < k:
+            if nums[i] == val:
+                nums[i] = nums[k-1]
+                k-=1
+            else:
+                i+=1
         return k
