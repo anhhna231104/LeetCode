a6566dd2acc3a906175a5c9c5d99ab33a9ba3783
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-03-28 22:37:54 +0700

    Time: 27 ms (87.6%), Space: 12.5 MB (16.94%) - LeetHub

--- 2752-sum-multiples/2752-sum-multiples.py
+++ 2752-sum-multiples/2752-sum-multiples.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         # return sum(num for num in range(1, n+1) if num%3==0 or num%5==0 or num%7==0)
 
-        selected = set()
+        multiples = set()
         for k in [3, 5, 7]:
             for num in range(k, n+1, k):
-                selected.add(num)
-        return sum(selected)
+                multiples.add(num)
+        return sum(multiples)
         
