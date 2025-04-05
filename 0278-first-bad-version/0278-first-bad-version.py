bed9aa95fa5ec1ebc5dc9fd619c3273d6e127b2c
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-04-05 20:50:25 +0700

    Time: 13 ms (71.55%), Space: 12.5 MB (51.12%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,27 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # if n==1:
+        #     return 1
+        # for i in range(1, n+1):
+        #     if isBadVersion(i):
+        #         return i
+
+        left, right = 1, n
+        while left<right:
+            mid = left + (right-left)//2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
+
+        
