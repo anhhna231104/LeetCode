7001159da0e519257288a24a78d7df61c9809aca
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-04-26 19:06:26 +0700

    Time: 14 ms (32.58%), Space: 13.6 MB (90.17%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count, major, i = 0, None, 0
+        while(i < len(nums)):
+            if count==0:
+                major=nums[i]
+                count+=1
+            elif major == nums[i]:
+                count+=1
+            else:
+                count-=1
+            i+=1
+        return major
+
+
+
+        
