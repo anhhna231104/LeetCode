d7d024cbf03b251a55be36128ab2735a50c37874
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-04-26 19:09:39 +0700

    Time: 0 ms (100%), Space: 13.7 MB (31.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,18 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count, major, i = 0, None, 0
-        while(i < len(nums)):
-            if count==0:
-                major=nums[i]
-                count+=1
-            elif major == nums[i]:
-                count+=1
-            else:
-                count-=1
-            i+=1
-        return major
+        # count, major, i = 0, None, 0
+        # while(i < len(nums)):
+        #     if count==0:
+        #         major=nums[i]
+        #         count+=1
+        #     elif major == nums[i]:
+        #         count+=1
+        #     else:
+        #         count-=1
+        #     i+=1
+        # return major
 
+        count, major = 0, None
+        for num in nums:
+            if count==0:
+                major = num
+                count +=1
+            elif major == num:
+                count+=1
+            else: 
+                count -=1
+        return major
 
 
         
