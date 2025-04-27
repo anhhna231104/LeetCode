9d866f4761fb1be08fb9e4b989d268850581d609
Nguyễn Ngọc Tú Anh <117065875+Ceyline@users.noreply.github.com>
2025-04-27 19:56:22 +0700

    Time: 0 ms (100%), Space: 12.5 MB (12.88%) - LeetHub

---
+++ 0851-goat-latin/0851-goat-latin.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def toGoatLatin(self, sentence):
+        """
+        :type sentence: str
+        :rtype: str
+        """
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        words = sentence.split()
+        goat_latin = []
+        for i,w in enumerate(words):
+            if w[0].lower() in vowels:
+                goat = w +'ma'
+            else:
+                goat = w[1:] + w[0] + 'ma'
+            goat+='a'*(i+1)
+            goat_latin.append(goat)
+        return join(goat_latin)
+
+
+
+        
