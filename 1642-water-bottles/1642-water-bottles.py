b620e5cc7fc2673bc6566531aa84fe5cef1d9657
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-10-27 13:57:24 +0700

    Time: 0 ms (100%), Space: 12.3 MB (79.64%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        """
+        :type numBottles: int
+        :type numExchange: int
+        :rtype: int
+        """
+        res, emptyBottles = 0, 0
+        while numBottles > 0:
+            res += numBottles
+            emptyBottles += numBottles
+            numBottles = emptyBottles // numExchange
+            emptyBottles = emptyBottles % numExchange
+
+        return res
+        
