86d560c9eef9123fe75ba3dabe906c1c1dea7a67
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-10-28 13:08:12 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.15%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -4,16 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        arr, taken = [], []
+        arr = []
+        nums.sort()
 
-        while nums:
-            min_val = min(nums)
-            taken.append(min_val)
-            nums.remove(min_val)
-
-        for i in range(0, len(taken), 2):
-            arr.append(taken[i+1])
-            arr.append(taken[i])
+        for i in range(0, len(nums), 2):
+            arr.append(nums[i+1])
+            arr.append(nums[i])            
 
         return arr
         
