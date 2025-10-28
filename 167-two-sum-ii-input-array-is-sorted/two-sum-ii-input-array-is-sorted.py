be3fab2d7b487e13e112ecb5e838b93969f34cd6
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-10-28 21:04:41 +0700

    Time: 3 ms (86.66%) | Memory: 13 MB (34.99%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return
+
+        map = {}
+
+        for i, num in enumerate(nums):
+            comp = target - num
+
+            if comp in map:
+                return [map[comp] + 1, i + 1]
+            map[num] = i
+        
+        return map
+
+
+        
