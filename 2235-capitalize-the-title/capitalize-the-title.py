8849a5ff0b8b69ccd93325a59bc2692c84b7e503
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-01 14:25:20 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.38%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def capitalizeTitle(self, title):
+        """
+        :type title: str
+        :rtype: str
+        """
+        words = title.split()
+
+        for i in range(len(words)):
+            w_len = len(words[i])
+
+            if w_len <= 2:
+                words[i] = words[i].lower()
+            else:
+                words[i] = words[i].capitalize()
+            
+        return " ". join(words)
+                
+        
