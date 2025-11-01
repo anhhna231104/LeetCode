5cccd6dde883b860a21ed4a005814038266834e9
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-01 21:36:24 +0700

    Time: 74 ms (29.30%) | Memory: 14 MB (6.37%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.py
@@ -6,15 +6,15 @@ class Solution(object):
         """
         n = len(matrix)
 
-        r_seen, c_seen = [[] for _ in range(n)], [[] for _ in range(n)]
+        r_seen, c_seen = [set() for _ in range(n)], [set() for _ in range(n)]
 
         for r in range(n):
             for c in range(n):
                 v = matrix[r][c]
                 if v in r_seen[r] or v in c_seen[c]:
                     return False
-                r_seen[r].append(v)
-                c_seen[c].append(v)
+                r_seen[r].add(v)
+                c_seen[c].add(v)
 
         return True
         
