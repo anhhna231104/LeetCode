87f0042fba23a9f0122ea9f40945dce6be4c16f7
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-11-02 16:47:26 +0700

    Time: 2 ms (42.11%) | Memory: 12.3 MB (85.47%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                res.append(i)
+        
+        return res
